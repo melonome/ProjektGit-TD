789fd18b165863d41fd52bbb97352c56cf9d0935
Monget Eudes <emonget@SA207-01.ensi-bourges.fr>
2016-05-18 11:33:47 +0200

    test commit depuis un clone

---
+++ repo-git/testGit.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char ** argv[])
+{
+	if(argc !=2)
+	{
+		printf("usage: %s nombre \n",argv[0]);	
+		return -1;
+	}
+	sleep(int(argv[1]));
+
+	printf("pause de %i secondes \n", argv[1]);
+	return 0;	
+}
+
